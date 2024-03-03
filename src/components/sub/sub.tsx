e86e92d70b9a9487629b6bcbdfeadc01c8ee5130
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-03-02 20:45:24 -0800

    few changes to sub

--- src/components/sub/sub.tsx
+++ src/components/sub/sub.tsx
@@ -4,7 +4,7 @@ import styles from './Sub.module.css'
 export default function Sub(){
     return(
         <div className={styles.sub}>
-            <h1 className={styles.h1}>Join PokeFormula today and become a Pokemon master!</h1>
+            <h1 className={styles.h1}>Become the ultimate Pokemon trainer with PokeFormula! Explore detailed stats and information for every Pokemon generation, compare stats, and engage in thrilling 1v1 battles. Plus, collect and play with Pokemon TCG cards won from battles!</h1>
             <div className={styles.spline}>
                 <Spline scene="https://prod.spline.design/5w3-WkVVLF44FsjH/scene.splinecode" />
             </div> 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,19 +22,20 @@ export default function App() {
         </div>
         <div className={styles.cardContainer}>
           <Card 
-            header="Collect TCG Cards" 
-            description="Win Pokemon TCG cards and build your collection!"
-            button="START COLLECTING"
-            >
-             <Spline scene="https://prod.spline.design/lUrSIMu6wGFLZSSN/scene.splinecode" />
+              header="Collect TCG Cards" 
+              description="Win Pokemon TCG cards and build your collection! Prices start at $5."
+              button="START COLLECTING"
+          >
+              <Spline scene="https://prod.spline.design/lUrSIMu6wGFLZSSN/scene.splinecode" />
           </Card>
+
           <Card
-            header="Battle 1v1"
-            description="Challenge others and test your strength!"
-            button="JOIN BATTLE"
-            >
-             <Spline scene="https://prod.spline.design/dYKF3Jmo0-vfzkzv/scene.splinecode" />
-          </Card>  
+              header="Battle 1v1"
+              description="Challenge others and test your strength! Entry fee: $10."
+              button="JOIN BATTLE"
+          >
+              <Spline scene="https://prod.spline.design/dYKF3Jmo0-vfzkzv/scene.splinecode" />
+          </Card>
         </div>
         <Sub />
         <Subcribe />
